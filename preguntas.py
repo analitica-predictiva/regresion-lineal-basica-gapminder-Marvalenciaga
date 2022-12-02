19ce6bf6309f554b6950da7176dc70307e64ce0c
María Carolina Valencia García <marvalenciaga@unal.edu.co>
2022-12-01 19:32:30 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -5,6 +5,7 @@ Regresión Lineal Univariada
 En este laboratio se construirá un modelo de regresión lineal univariado.
 
 """
+import numpy as np
 import pandas as pd
 
 
@@ -137,35 +138,45 @@ def pregunta_03():
 
 def pregunta_04():
     """
-    Evaluación del modelo
-    -------------------------------------------------------------------------------------
+    Particionamiento del conjunto de datos usando train_test_split.
+    Complete el código presentado a continuación.
     """
 
+    # Importe LinearRegression
+    # Importe train_test_split
     # Importe mean_squared_error
+    from sklearn.model_selection import train_test_split
+    from sklearn.linear_model import LinearRegression
     from sklearn.metrics import mean_squared_error
 
-    # Obtenga el pipeline optimo de la pregunta 3.
-    gridSearchCV = pregunta_03()
+    # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
+    df = pd.read_csv("gm_2008_region.csv")
 
-    # Cargue las variables.
-    X_train, X_test, y_train, y_test = pregunta_02()
+    # Asigne a la variable los valores de la columna `fertility`
+    X_fertility = df["fertility"].values.reshape(-1,1)
 
-    # Evalúe el modelo con los conjuntos de entrenamiento y prueba.
-    y_train_pred = gridSearchCV.predict(X_train)
-    y_test_pred = gridSearchCV.predict(X_test)
+    # Asigne a la variable los valores de la columna `life`
+    y_life = df["life"].values.reshape(-1,1)
 
-    # Compute el error cuadratico medio de entrenamiento y prueba. Redondee los
-    # valores a dos decimales.
+    # Divida los datos de entrenamiento y prueba. La semilla del generador de números
+    # aleatorios es 53. El tamaño de la muestra de entrenamiento es del 80%
+    (X_train, X_test, y_train, y_test,) = train_test_split(
+        X_fertility,
+        y_life,
+        test_size=0.2,
+        random_state=53,
+    )
 
-    mse_train = mean_squared_error(
-        y_train,
-        y_train_pred,
-    ).round(2)
+    # Cree una instancia del modelo de regresión lineal
+    LR = LinearRegression()
 
-    mse_test = mean_squared_error(
-        y_test,
-        y_test_pred,
-    ).round(2)
+    # Entrene el clasificador usando X_train y y_train
+    LR.fit(X_train, y_train)
 
-    # Retorne el error cuadrático medio para entrenamiento y prueba
-    return mse_train, mse_test
+    # Pronostique y_test usando X_test
+    y_pred = LR.predict(X_test)
+
+    # Compute and print R^2 and RMSE
+    print("R^2: {:6.4f}".format(LR.score(X_test, y_test)))
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    print("Root Mean Squared Error: {:6.4f}".format(rmse))
